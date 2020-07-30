5dddc85a81a9994009a3ed91a6409c467ee2f6d9
jonjongao <jonjon.liho@gmail.com>
2020-07-30 17:21:53 +0800

    Move JointMask from ChironServer

---
+++ Runtime/JointMask.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+[Flags]
+public enum JointMask
+{
+    None = 0,
+    All = Hip | UpperBody | RightArm | LeftArm |
+        LowerBody | RightLeg | LeftLeg,
+    Hip = 1 << 0,
+    UpperBody = 1 << 1,
+    RightArm = 1 << 2,
+    LeftArm = 1 << 3,
+    LowerBody = 1 << 4,
+    RightLeg = 1 << 5,
+    LeftLeg = 1 << 6
+}
